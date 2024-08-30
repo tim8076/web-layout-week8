baac414c1925b0f286186ca454a8e2fc2ee6c801
shosetim <shosetim32@gmail.com>
2024-08-30 12:03:26 +0800

    Updates

---
+++ assets/course-content-e4923bee.js
@@ -0,0 +1,105 @@
+import"./main-b8e2cc44.js";const t=[{title:{content:"課前說明",chapterNum:"4 個單元",time:"少於 1 分鐘"},list:[{name:"課程說明",progress:"00:10"},{name:"課程最終任務說明",progress:"00:20"},{name:"課程說明",progress:"00:30"},{name:"Hahow 學生如何在六角平台檢視問題",progress:"00:40"},{name:"課業作業",progress:"00:50"}]},{title:{content:"2020 VS Code 網頁編輯器、HTML 基礎教學",chapterNum:"15 個單元",time:"25 分鐘"},list:[{name:"課程說明",progress:"00:10"},{name:"課程最終任務說明",progress:"00:20"},{name:"課程說明",progress:"00:30"},{name:"Hahow 學生如何在六角平台檢視問題",progress:"00:40"},{name:"課業作業",progress:"00:50"}]},{title:{content:"2020 CSS 常用語法",chapterNum:"9 個單元",time:"37 分鐘"},list:[{name:"課程說明",progress:"00:10"},{name:"課程最終任務說明",progress:"00:20"},{name:"課程說明",progress:"00:30"},{name:"Hahow 學生如何在六角平台檢視問題",progress:"00:40"},{name:"課業作業",progress:"00:50"}]},{title:{content:"2020 使用 CSS 變更 HTML 標籤特性",chapterNum:"13 個單元",time:"52 分鐘"},list:[{name:"課程說明",progress:"00:10"},{name:"課程最終任務說明",progress:"00:20"},{name:"課程說明",progress:"00:30"},{name:"Hahow 學生如何在六角平台檢視問題",progress:"00:40"},{name:"課業作業",progress:"00:50"}]},{title:{content:"Flex 網頁排版技巧",chapterNum:"13 個單元",time:"26 分鐘"},list:[{name:"課程說明",progress:"00:10"},{name:"課程最終任務說明",progress:"00:20"},{name:"課程說明",progress:"00:30"},{name:"Hahow 學生如何在六角平台檢視問題",progress:"00:40"},{name:"課業作業",progress:"00:50"}]},{title:{content:"Flex 精神時光屋",chapterNum:"8 個單元",time:"1 小時 15 分鐘"},list:[{name:"課程說明",progress:"00:10"},{name:"課程最終任務說明",progress:"00:20"},{name:"課程說明",progress:"00:30"},{name:"Hahow 學生如何在六角平台檢視問題",progress:"00:40"},{name:"課業作業",progress:"00:50"}]},{title:{content:"章節說明",chapterNum:"1 個單元",time:"少於 1 分鐘"},list:[{name:"課程說明",progress:"00:10"},{name:"課程最終任務說明",progress:"00:20"},{name:"課程說明",progress:"00:30"},{name:"Hahow 學生如何在六角平台檢視問題",progress:"00:40"},{name:"課業作業",progress:"00:50"}]},{title:{content:"網頁排版技巧 Part II",chapterNum:"7 個單元",time:"24 分鐘"},list:[{name:"課程說明",progress:"00:10"},{name:"課程最終任務說明",progress:"00:20"},{name:"課程說明",progress:"00:30"},{name:"Hahow 學生如何在六角平台檢視問題",progress:"00:40"},{name:"課業作業",progress:"00:50"}]},{title:{content:"切圖技巧",chapterNum:"12 個單元",time:"35 分鐘"},list:[{name:"課程說明",progress:"00:10"},{name:"課程最終任務說明",progress:"00:20"},{name:"課程說明",progress:"00:30"},{name:"Hahow 學生如何在六角平台檢視問題",progress:"00:40"},{name:"課業作業",progress:"00:50"}]},{title:{content:"表格與表單設計技巧",chapterNum:"10 個單元",time:"30 分鐘"},list:[{name:"課程說明",progress:"00:10"},{name:"課程最終任務說明",progress:"00:20"},{name:"課程說明",progress:"00:30"},{name:"Hahow 學生如何在六角平台檢視問題",progress:"00:40"},{name:"課業作業",progress:"00:50"}]},{title:{content:"CSS3 技巧",chapterNum:"5 個單元",time:"23 分鐘"},list:[{name:"課程說明",progress:"00:10"},{name:"課程最終任務說明",progress:"00:20"},{name:"課程說明",progress:"00:30"},{name:"Hahow 學生如何在六角平台檢視問題",progress:"00:40"},{name:"課業作業",progress:"00:50"}]},{title:{content:"CSS3 技巧",chapterNum:"5 個單元",time:"23 分鐘"},list:[{name:"課程說明",progress:"00:10"},{name:"課程最終任務說明",progress:"00:20"},{name:"課程說明",progress:"00:30"},{name:"Hahow 學生如何在六角平台檢視問題",progress:"00:40"},{name:"課業作業",progress:"00:50"}]},{title:{content:"最後，讓你的網頁變得更專業",chapterNum:"10 個單元",time:"少於 18 分鐘"},list:[{name:"課程說明",progress:"00:10"},{name:"課程最終任務說明",progress:"00:20"},{name:"課程說明",progress:"00:30"},{name:"Hahow 學生如何在六角平台檢視問題",progress:"00:40"},{name:"課業作業",progress:"00:50"}]},{title:{content:"開發多頁式網站實戰講解",chapterNum:"6 個單元",time:"8 分鐘"},list:[{name:"課程說明",progress:"00:10"},{name:"課程最終任務說明",progress:"00:20"},{name:"課程說明",progress:"00:30"},{name:"Hahow 學生如何在六角平台檢視問題",progress:"00:40"},{name:"課業作業",progress:"00:50"}]},{title:{content:"接下來，你還可以學什麼？",chapterNum:"3 個單元",time:"少於 1 分鐘"},list:[{name:"課程說明",progress:"00:10"},{name:"課程最終任務說明",progress:"00:20"},{name:"課程說明",progress:"00:30"},{name:"Hahow 學生如何在六角平台檢視問題",progress:"00:40"},{name:"課業作業",progress:"00:50"}]}],n={courseChapterData:t},o=document.querySelector("[data-course-list-container]"),i=n.courseChapterData,p=()=>{let a="";i.forEach((r,s)=>{const{list:e}=r;a+=`
+      <div class="accordion mb-4" id="accordian-course">
+        <div class="accordion-item">
+          <h3 class="accordion-header">
+            <button class="accordion-button d-flex justify-content-between ${s===0?"":"collapsed"}"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapse-${s}"
+              aria-expanded="true"
+              aria-controls="collapseOne">
+              <span class="w-24p h-24p bg-gray-80 text-light d-flex justify-content-center align-items-center rounded-1 me-2">
+                ${s+1}
+              </span>
+              ${r.title.content}
+              <p class="fs-8 position-absolute top-50 end-65p translate-middle-y
+                d-none d-lg-block">
+                ${r.title.chapterNum}
+                <span class="d-inline-block mx-2 w-6p h-6p bg-gray-60 rounded-circle"></span>
+                ${r.title.time}
+              </p>
+            </button>
+          </h3>
+          <div id="collapse-${s}" class="accordion-collapse collapse ${s===0?"show":""}"
+            data-bs-parent="#accordian-course">
+            <div class="accordion-body">
+              <ul class="list-unstyled m-0">
+                <li class="text-gray-80">
+                  <a href="#" class="d-flex justify-content-between align-items-center px-6 py-4
+                    bg-primary-10-hover">
+                    <div class="d-flex align-items-center">
+                      <img src="../assets/images/icons/ic_video.svg"
+                        alt="video-icon"
+                        class="w-24p h-24p me-3">
+                      <p>${e[0].name}</p>
+                    </div>
+                    <div class="d-flex align-items-center ms-4">
+                      <div class="py-1 px-2 bg-primary text-light rounded-2 fs-9 me-4">
+                        試看單元
+                      </div>
+                      <p class="fs-7">${e[0].progress}</p>
+                    </div>
+                  </a>
+                </li>
+                <li class="text-gray-80">
+                  <a href="#" class="d-flex justify-content-between align-items-center px-6 py-4
+                    bg-primary-10-hover">
+                    <div class="d-flex align-items-center">
+                      <img src="../assets/images/icons/ic_video.svg"
+                        alt="video-icon"
+                        class="w-24p h-24p me-3">
+                      <p>${e[1].name}</p>
+                    </div>
+                    <div class="d-flex align-items-center ms-4">
+                      <p class="fs-7">${e[0].progress}</p>
+                    </div>
+                  </a>
+                </li>
+                <li class="text-gray-80">
+                  <a href="#" class="d-flex justify-content-between align-items-center px-6 py-4
+                    bg-primary-10-hover">
+                    <div class="d-flex align-items-center">
+                      <img src="../assets/images/icons/ic_video.svg"
+                        alt="video-icon"
+                        class="w-24p h-24p me-3">
+                      <p>${e[2].name}</p>
+                    </div>
+                    <div class="d-flex align-items-center ms-4">
+                      <p class="fs-7">${e[0].progress}</p>
+                    </div>
+                  </a>
+                </li>
+                <li class="text-gray-80">
+                  <a href="#" class="d-flex justify-content-between align-items-center px-6 py-4
+                    bg-primary-10-hover">
+                    <div class="d-flex">
+                      <img src="../assets/images/icons/ic_video.svg"
+                        alt="video-icon"
+                        class="w-24p h-24p me-3">
+                      <p>${e[3].name}</p>
+                    </div>
+                    <div class="d-flex align-items-center ms-4">
+                      <p class="fs-7">${e[0].progress}</p>
+                    </div>
+                  </a>
+                </li>
+                <li class="text-gray-80">
+                  <a href="#" class="d-flex justify-content-between align-items-center px-6 py-4
+                    bg-primary-10-hover">
+                    <div class="d-flex align-items-center">
+                      <img src="../assets/images/icons/ic_video.svg"
+                        alt="video-icon"
+                        class="w-24p h-24p me-3">
+                      <p>${e[4].name}</p>
+                    </div>
+                    <div class="d-flex align-items-center ms-4">
+                      <p class="fs-7">${e[0].progress}</p>
+                    </div>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    `}),o.innerHTML=a};p();
