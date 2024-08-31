c938d20e7f6a7ca836c2ce86bc97ec204f01211f
shosetim <shosetim32@gmail.com>
2024-08-31 13:56:50 +0800

    完成訂單通知頁面切版

---
+++ assets/js/order-complete.js
@@ -0,0 +1,26 @@
+// import Swiper JS
+import Swiper from 'swiper';
+import { Autoplay } from "swiper/modules";
+
+const swiperCategory = new Swiper('.swiper-more-course', {
+  modules: [Autoplay],
+  // Optional parameters
+  slidesPerView: 1.2,
+  spaceBetween: 24,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2.2,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+    1296: {
+      slidesPerView: 4,
+    },
+  }
+});
